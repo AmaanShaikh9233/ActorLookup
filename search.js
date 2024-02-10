d45188b534f32eec26f4b1a2e3e33f699a61367f
Amaan shaikh <154674755+AmaanShaikh9233@users.noreply.github.com>
2024-02-11 04:44:34 +0530

    Update search.js

--- search.js
+++ search.js
@@ -1,4 +1,7 @@
 const { findActorByMoviesName, findMoviesByActorName } = require('./actorLookup.js');
 
 console.log("Actors worked in movie:",findActorByMoviesName("Pathaan"));
+
 console.log("Movies of the searched Actor is:",findMoviesByActorName("Salman Khan"));
+
+// Check the values of Movies and Actors according to your choice
